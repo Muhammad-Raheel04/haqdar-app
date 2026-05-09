0f15048c0b9506ed274b47cfda7dc40f8c2df2be
Muhammad Raheel <raheel31102004@gmail.com>
2026-05-09 16:57:08 +0500

    fix(haqdarRoutes): fix get route to post route for /get-response end point

--- backend/routes/haqdarRoutes.js
+++ backend/routes/haqdarRoutes.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { haqdar } from '../controllers/haqdarController.js';
 const router=express.Router();
 
-router.get('/get-response',haqdar);
+router.post('/get-response',haqdar);
 
 export default router;
