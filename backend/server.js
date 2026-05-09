f9c7c44966826988e42e335f09189f0fa5082ed6
Muhammad Raheel <raheel31102004@gmail.com>
2026-05-09 16:57:49 +0500

    feat(server): add cors middleware to allow cross origin requests

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import haqdarRoutes from './routes/haqdarRoutes.js';
+import cors from 'cors';
 const app=express();
 const PORT=process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/v1',haqdarRoutes);
 app.listen(PORT,()=>{
     console.log(`haqdar server listening at http://localhost:${PORT}`)
